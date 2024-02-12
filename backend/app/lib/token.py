8a3fb450b163ea5b2c47ede81983bea4cf234864
wenMoonx <supertalented730@gmail.com>
2024-02-12 23:30:14 +0900

    fix: update token list

--- backend/app/lib/token.py
+++ backend/app/lib/token.py
@@ -1,9 +1,9 @@
 from typing import Sequence
 from app.schemas.token import Token
+from app.core.conf import settings
 
-
-def get_token() -> Sequence[Token]:
-    return [
+tokens = {
+    97: [
         Token(
             address="0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684",
             name="Tether",
@@ -17,7 +17,27 @@ def get_token() -> Sequence[Token]:
             decimals=18,
             symbol="BUSD"
         )
+    ],
+    56: [
+        Token(
+            address="0x63b7e5aE00cc6053358fb9b97B361372FbA10a5e",
+            name="Tether",
+            decimals=6,
+            symbol="USDT"
+        ),
+        Token(
+            address="0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56",
+            # address="0x78867BbEeF44f2326bF8DDd1941a4439382EF2A6",
+            name="BUSD",
+            decimals=18,
+            symbol="BUSD"
+        )
     ]
+}
+
+
+def get_token() -> Sequence[Token]:
+    return tokens[settings.CHAIN_ID]
 
 
 def get_token_by_address(address: str) -> Token:
