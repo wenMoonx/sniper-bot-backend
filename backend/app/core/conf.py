5c2d4c49a0d951227f88e30ae7d62279a8dcb39a
wenMoonx <supertalented730@gmail.com>
2024-02-21 23:06:50 +0900

    fix update rpc url

--- backend/app/core/conf.py
+++ backend/app/core/conf.py
@@ -42,7 +42,7 @@ class Settings(BaseSettings):
 
     # WEB3
     # RPC_SERVER_URL: str = 'https://bsc-testnet.publicnode.com'
-    RPC_SERVER_URL: str = 'https://binance.llamarpc.com'
+    RPC_SERVER_URL: str = 'https://bsc.rpc.blxrbdn.com'
     # CHAIN_ID: int = 97
     CHAIN_ID: int = 56
     WALLET_PREFIX: str = 'KEYSMASH FJAFJKLDSKF7JKFDJ 1530'
