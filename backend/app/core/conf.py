6d16c6082ca7e75783235245bbac6cc3eb0475c4
wenMoonx <supertalented730@gmail.com>
2024-02-21 23:10:33 +0900

    fix update rpc url

--- backend/app/core/conf.py
+++ backend/app/core/conf.py
@@ -42,7 +42,7 @@ class Settings(BaseSettings):
 
     # WEB3
     # RPC_SERVER_URL: str = 'https://bsc-testnet.publicnode.com'
-    RPC_SERVER_URL: str = 'https://bsc.rpc.blxrbdn.com'
+    RPC_SERVER_URL: str = 'https://bsc-mainnet.public.blastapi.io'
     # CHAIN_ID: int = 97
     CHAIN_ID: int = 56
     WALLET_PREFIX: str = 'KEYSMASH FJAFJKLDSKF7JKFDJ 1530'
